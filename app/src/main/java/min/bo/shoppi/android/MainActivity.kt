081a578d816089306051f43a1615d745d54c6f5f
Eddy <min06274@naver.com>
2023-02-17 15:49:59 +0900

    Bottom Navigation 추가

--- app/src/main/java/min/bo/shoppi/android/MainActivity.kt
+++ app/src/main/java/min/bo/shoppi/android/MainActivity.kt
@@ -2,10 +2,14 @@ package min.bo.shoppi.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null
     }
 }
