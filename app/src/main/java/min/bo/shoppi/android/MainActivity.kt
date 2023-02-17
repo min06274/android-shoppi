fce79a6ed0d8b4966634d1a2c47d5454931700ad
Eddy <min06274@naver.com>
2023-02-17 18:29:35 +0900

    Jetpack Navigation , let 오류 해결 (강사랑 gradle 똑같이 함)

--- app/src/main/java/min/bo/shoppi/android/MainActivity.kt
+++ app/src/main/java/min/bo/shoppi/android/MainActivity.kt
@@ -2,6 +2,8 @@ package min.bo.shoppi.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,12 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
         bottomNavigationView.itemIconTintList = null
+
+
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let{
+            bottomNavigationView.setupWithNavController(it)
+        }
+
     }
 }
