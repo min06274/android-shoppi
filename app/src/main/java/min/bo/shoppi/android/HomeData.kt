4b7eaddf543dff1c6b631db349a6b8d2940002f6
Eddy <min06274@naver.com>
2023-02-27 22:42:17 +0900

    viewPager2 사용해서 화면 스와이프 구현 (gson 사용법도)

---
+++ app/src/main/java/min/bo/shoppi/android/HomeData.kt
@@ -0,0 +1,9 @@
+package min.bo.shoppi.android
+
+import com.google.gson.annotations.SerializedName
+
+data class HomeData(
+    val title:Title,
+    @SerializedName("top_banners") val topBanners: List<Banner>
+
+)
--- app/src/main/java/min/bo/shoppi/android/HomeFragment.kt
+++ app/src/main/java/min/bo/shoppi/android/HomeFragment.kt
@@ -1,15 +1,15 @@
 package min.bo.shoppi.android
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.gson.Gson
 import org.json.JSONObject
 
 class HomeFragment:Fragment() {
@@ -28,25 +28,29 @@ class HomeFragment:Fragment() {
 
         val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
         val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
+        val viewpager = view.findViewById<ViewPager2>(R.id.viewpager_home_banner)
+        val viewpagerIndicator = view.findViewById<TabLayout>(R.id.viewpager_home_banner_indicator)
 
 
         val assetLoader = AssetLoader()
-        val homeData = assetLoader.getJsonString(requireContext(),"home.json")
+        val homeJsonString = assetLoader.getJsonString(requireContext(),"home.json")
 
 
-        if(!homeData.isNullOrEmpty()){
-            val jsonObject = JSONObject(homeData)
+        if(!homeJsonString.isNullOrEmpty()){
+            val gson = Gson()
+            val homeData = gson.fromJson(homeJsonString,HomeData::class.java)
 
 
-            val title = jsonObject.getJSONObject("title")
-            val text = title.getString("text")
-            val iconUrl = title.getString("icon_url")
-
-            toolbarTitle.text = text
+            toolbarTitle.text = homeData.title.text
             GlideApp.with(this)
-                .load(iconUrl)
+                .load(homeData.title.iconUrl)
                 .into(toolbarIcon)
 
+
+
+            viewpager.adapter=HomeBannerAdapter().apply {
+                submitList(homeData.topBanners)
+            }
         }
     }
 }
